e7805125ce2b5176e65a7184949246c40991af67
Сергей Костяев <s.kostyaev@office.ngs.ru>
2015-04-30 17:07:57 +0600

    fix i3status

--- my_i3status.sh
+++ my_i3status.sh
@@ -4,6 +4,6 @@
       i3status | while :
         do
 			read line
-			echo "`amixer sget Master | tail -n1 | awk '{if ($6 == "[on]") print " " $4 ; else print ""}'` | $line" || exit 1
+			echo "`amixer sget Master | tail -n1 | awk '{if ($6 == "[on]") print " " $5 ; else print ""}'` | $line" || exit 1
 		done
 			
