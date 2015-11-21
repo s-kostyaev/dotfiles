28f547d40d4c33cb11fc39bd101d519a9162f165
Sergey Kostyaev <s.kostyaev@office.ngs.ru>
2015-11-22 05:26:04 +0600

    add web building env

--- install-prog-tools.sh
+++ install-prog-tools.sh
@@ -4,3 +4,9 @@ echo installing some tools for web development
 sudo npm install -g eslint babel-eslint eslint-plugin-react
 echo install tern for intelligent js
 sudo npm install -g tern
+echo install serve for see frontend in browser
+sudo npm install -g serve
+echo install webpack for building javascript with deps
+sudo npm install -g webpack
+echo install webpack-dev-server for autoreload changed files
+sudo npm install -g webpack-dev-server
