f74e0def0ce2252cff57cfa7bf75ec354aa4cbbe
Sergey Kostyaev <sergey.kostyaev@eltex.loc>
2016-09-20 10:25:57 +0700

    fix emacs scrolling

--- .keyboard.sh
+++ .keyboard.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
-#setxkbmap -option ctrl:nocaps
-#setxkbmap "us,ru" ",winkeys" "grp:toggle,grp_led:caps"
-killall -9 xcape
-./reconfig-kbd
-./space2hyper
+setxkbmap -option ctrl:nocaps
+setxkbmap "us,ru" ",winkeys" "grp:toggle,grp_led:caps"
+# killall -9 xcape
+# ./reconfig-kbd
+# ./space2hyper
+
+xset r rate 200 60
+xset -b
+
+HANDLER_ENG="CAPSLOCK"
+HANDLER_RUS="RIGHTSHIFT"
+if grep -q "Razer" /proc/bus/input/devices; then
+    KEYBOARD="razer"
+else
+    KEYBOARD="HID 04f3:0103"
+fi
+
+sudo pkill shift-shift
+sudo shift-shift -quiet=true \
+    -match "$KEYBOARD" -first "$HANDLER_ENG" -second "$HANDLER_RUS" &
