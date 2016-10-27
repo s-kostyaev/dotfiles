7ae4588b7903f23970e3a9c0cb3f7eca06b87883
Sergey Kostyaev <feo.me@ya.ru>
2016-10-27 23:45:14 +0700

    fix i3status

--- my_i3status.sh
+++ my_i3status.sh
@@ -4,6 +4,6 @@
       i3status | while :
         do
 			read line
-			echo "`amixer sget Master | tail -n1 | awk '{if ($6 == "[on]") print " " $5 ; else print ""}'` | $line" || exit 1
+			echo "`pamixer --get-mute  | awk '{if ($1 == "false") print " " ; else print ""}'` `pamixer --get-volume` | $line" || exit 1
 		done
 			
