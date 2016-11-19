8eb195583e9f9a97bead37a5303d6d3a048bfec5
Sergey Kostyaev <feo.me@ya.ru>
2016-11-20 02:32:35 +0700

    add monitor hotplug

--- .display.sh
+++ .display.sh
@@ -1,2 +1,8 @@
-#!/bin/bash
-#xrandr --output HDMI2 --mode 1920x1080 --pos 0x0 --output VGA1  --mode 1920x1080 --pos 1920x0 --rotate left
+#!/bin/sh
+echo "$SRANDRD_ACTION"
+case "$SRANDRD_ACTION" in
+    # "DVI-0 connected") xrandr --output DVI-0 --auto --right-of LVDS;;
+    # "VGA-0 connected") xrandr --output VGA-0 --auto --left-of LVDS;;
+    "VGA-1 disconnected") xrandr --output LVDS-1 --mode 1366x768 --pos 0x0 --output VGA-1 --off;;
+    "VGA-1 connected") xrandr --output LVDS-1 --mode 1366x768 --pos 0x0 --output VGA-1 --mode 1440x900 --pos 1366x0;;
+esac
