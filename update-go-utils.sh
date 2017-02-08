fa8ba076e44630057303b2d73b587f4a54d522dc
Sergey Kostyaev <sergey.kostyaev@eltex.loc>
2017-02-08 15:42:46 +0700

    remove unused go packages

--- update-go-utils.sh
+++ update-go-utils.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
 
 PKGS='github.com/github/hub
-github.com/kovetskiy/manul
-github.com/seletskiy/hastur
-github.com/seletskiy/ash
-github.com/reconquest/gunter
-golang.org/x/tools/cmd/oracle
-github.com/svent/sift
 github.com/kisielk/errcheck
 github.com/golang/lint/golint
-github.com/reconquest/marvex
 github.com/seletskiy/go-makepkg
 github.com/nsf/gocode
-github.com/tears-of-noobs/batrak
 github.com/rogpeppe/godef
 github.com/dougm/goflymake
 github.com/alecthomas/gometalinter
