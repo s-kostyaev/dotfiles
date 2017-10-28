72480f0aa7365e5203d1369c54b020c7cc09ff1b
Sergey Kostyaev <feo.me@ya.ru>
2017-10-29 01:47:54 +0700

    fix updating go-tools

--- update-go-utils.sh
+++ update-go-utils.sh
@@ -2,24 +2,13 @@
 
 PKGS='github.com/github/hub
 github.com/kisielk/errcheck
-github.com/golang/lint/golint
 github.com/seletskiy/go-makepkg
 github.com/nsf/gocode
 github.com/rogpeppe/godef
-github.com/dougm/goflymake
 github.com/alecthomas/gometalinter
 golang.org/x/tools/cmd/goimports
-github.com/fzipp/gocyclo
 github.com/josharian/impl
 github.com/maruel/panicparse
-github.com/remyoudompheng/go-misc/deadcode
-github.com/gordonklaus/ineffassign
-github.com/mvdan/interfacer/cmd/interfacer
-github.com/opennota/check/cmd/aligncheck
-github.com/opennota/check/cmd/structcheck
-github.com/opennota/check/cmd/varcheck
-golang.org/x/tools/cmd/gotype
-github.com/mibk/dupl
 github.com/zmb3/gogetdoc
 github.com/godoctor/godoctor
 github.com/jstemmer/gotags
@@ -29,8 +18,12 @@ github.com/stapelberg/expanderr
 github.com/posener/complete/gocomplete
 github.com/sourcegraph/go-langserver
 github.com/cweill/gotests/...
+github.com/davidrjenni/reftools/...
 '
 
 for pkg in $PKGS; do echo updating $pkg; go get -u $pkg; done
 
+echo "installing gocomplete"
 gocomplete -install -y
+echo "updating linters"
+gometalinter --install --update
