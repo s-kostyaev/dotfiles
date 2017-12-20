699877ed60478256317e26bc6ee20f892462ad72
Sergey Kostyaev <feo.me@ya.ru>
2017-12-21 00:12:05 +0700

    add alias gh

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,6 @@
 set __GIT_PROMPT_DIR ~/bash-git-prompt
 alias et "emacsclient -t"
+alias gh 'git log --pretty=format:"%h %ad | %s%d [%an]" --graph --date=short'
 if not functions -q fundle; eval (curl -sfL https://git.io/fundle-install); end
 fundle plugin hagiyat/anyfff
 
