53f0f5181ee6450d3ad760b6c30b3a55d165ecb7
Sergey Kostyaev <s.kostyaev@tinkoff.ru>
2017-12-21 14:01:17 +0700

    change put filename keybinding

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -2,6 +2,6 @@ function fish_user_key_bindings
          emit fish_user_key_bindings
 
          bind \cr 'anyfff_widget put_history'
-         bind \cf 'anyfff_widget put_filename'
+         bind \cx\cf 'anyfff_widget put_filename'
          bind \cx\cb 'anyfff_widget put_git_branch'
  end
