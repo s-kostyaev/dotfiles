75e79f73cfffbcd93c94b60dd54935903fcdb79a
Sergey Kostyaev <sergey.kostyaev@bitfury.com>
2018-12-26 18:10:08 +0700

    update go utils list

--- update-go-utils.sh
+++ update-go-utils.sh
@@ -2,21 +2,13 @@
 
 # github.com/mdempsky/gocode
 
-PKGS='github.com/github/hub
-github.com/seletskiy/go-makepkg
-github.com/stamblerre/gocode
-github.com/rogpeppe/godef
+PKGS='github.com/stamblerre/gocode
 golang.org/x/tools/cmd/goimports
 github.com/josharian/impl
-github.com/maruel/panicparse
-github.com/zmb3/gogetdoc
-github.com/godoctor/godoctor
 github.com/jstemmer/gotags
 github.com/derekparker/delve/cmd/dlv
 github.com/ValeLint/vale
-github.com/stapelberg/expanderr
 github.com/posener/complete/gocomplete
-github.com/sourcegraph/go-langserver
 github.com/cweill/gotests/...
 github.com/davidrjenni/reftools/...
 github.com/kovetskiy/transline
@@ -26,7 +18,8 @@ github.com/google/codesearch/cmd/...
 github.com/peco/peco/...
 github.com/golang/dep/cmd/dep
 github.com/justjanne/powerline-go
-github.com/golangci/golangci-lint/cmd/golangci-lint
+github.com/isacikgoz/tldr
+honnef.co/go/tools/cmd/megacheck
 '
 
 for pkg in $PKGS; do echo updating $pkg; go get -u $pkg; done
